d6c29ed4b058c61d1f700b31c00cde128cbfbe57
USUARIO-PC\USUARIO <majitobella_star@hotmail.com>
2016-09-07 03:21:33 -0500

    commit #1

---
+++ src/integracioncontinua/rubrica32/IntegracioncontinuaRubrica32.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integracioncontinua.rubrica32;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class IntegracioncontinuaRubrica32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+         System.out.println("Bienvenido a Github -- hola mundo-- soy Majo");
+    }
+    
+}
